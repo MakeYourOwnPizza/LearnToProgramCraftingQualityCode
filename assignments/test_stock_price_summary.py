ed3493227aa8fe9eecbf60334ba15110ba774312
MakeYourOwnPizza <34776179+MakeYourOwnPizza@users.noreply.github.com>
2018-01-19 14:36:56 -0600

    test_stock_price_smmary.py correction

--- assignments/test_stock_price_summary.py
+++ assignments/test_stock_price_summary.py
@@ -12,49 +12,28 @@ class TestStockPriceSummary(unittest.TestCase):
         expected = (0, 0)
         self.assertEqual(expected, actual)
         
-    def test_stock_price_summary_example_1(self):
+    def test_stock_price_summary_single_zero(self):
         """Test stock_price_summary when there are only zeros."""
 
-        actual = a1.stock_price_summary([0, 0])
+        actual = a1.stock_price_summary([0])
         expected = (0, 0)
         self.assertEqual(expected, actual)
         
-    def test_stock_price_summary_example_2(self):
+    def test_stock_price_summary_single_positive(self):
         """Test stock_price_summary when there are only gains."""
 
-        actual = a1.stock_price_summary([0.01, 0.03, 0.10])
-        expected = (0.14, 0)
+        actual = a1.stock_price_summary([0.01])
+        expected = (0.01, 0)
         self.assertEqual(expected, actual)
         
-    def test_stock_price_summary_example_3(self):
+    def test_stock_price_summary_single_negative(self):
         """Test stock_price_summary when there are only losses."""
 
-        actual = a1.stock_price_summary([-0.02, -0.14, -0.01])
-        expected = (0, -0.17)
+        actual = a1.stock_price_summary([-0.02])
+        expected = (0, -0.02)
         self.assertEqual(expected, actual)
-        
-    def test_stock_price_summary_example_4(self):
-        """Test stock_price_summary when there are gains and losses."""
 
-        actual = a1.stock_price_summary([0.01, 0.03, -0.02, -0.14, 0.10, -0.01])
-        expected = (0.14, -0.17)
-        self.assertEqual(expected, actual)
-        
-    def test_stock_price_summary_example_5(self):
-        """Test stock_price_summary when the there are only gains and zeros"""
-
-        actual = a1.stock_price_summary([0.01, 0.03, 0, 0, 0.10])
-        expected = (0.14, 0)
-        self.assertEqual(expected, actual)
-        
-    def test_stock_price_summary_example_6(self):
-        """Test stock_price_summary when there are only losses and zeros."""
-
-        actual = a1.stock_price_summary([-0.02, -0.14, 0, 0, -0.01])
-        expected = (0, -0.17)
-        self.assertEqual(expected, actual)
-        
-    def test_stock_price_summary_example_7(self):
+    def test_stock_price_summary_mixed(self):
         """Test stock_price_summary when there are gains, losses, and zeros."""
 
         actual = a1.stock_price_summary([0.01, 0.03, -0.02, -0.14, 0, 0, 0.10, -0.01])
